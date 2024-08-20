7b268090b805c5ec8bac521f54815f8041fa2e33
itaynvn-runai <165032271+itaynvn-runai@users.noreply.github.com>
2024-08-20 19:50:53 +0300

    Update runapy_delete_department.py

--- runapy_delete_department.py
+++ runapy_delete_department.py
@@ -9,36 +9,42 @@ def get_project_info(project_id):
     project = client.projects.get(int(project_id))
     return f"project '{project['name']}' (ID: {project['id']}) in department '{project['parent']['name']}' (ID: {project['parent']['id']})"
 
-# select department by name
-departments = client.departments.all()['departments']
-for department in departments:
-    if department['name'] == dept_name:
-        my_dept = client.departments.get(int(department['id']))
-        print(f"found department '{my_dept['name']}', id: {my_dept['id']}")
+# define list of department names to delete
+department_list = ["dep1", "dep2", "dep3"]
+
+for department in department_list:
+    print(f"Starting script for department '{department}'...")
+
+    # select department by name
+    all_departments = client.departments.all()['departments']
+    for x in all_departments:
+        if x['name'] == department:
+            my_dept = client.departments.get(int(x['id']))
+            print(f"found department '{my_dept['name']}', id: {my_dept['id']}")
+            break
         break
-    break
 
-# create a list of projects in selected department
-projects_in_department = []
-projects_counter = 0
-projects = client.projects.all()['projects']
-for project in projects:
-    if project['parentId'] == my_dept['id']:
-        projects_in_department.append(project)
-        print(f"found {get_project_info(project['id'])}")
-        projects_counter += 0
-print(f"found {projects_counter} projects.")
-
-# delete all projects from list
-for project in projects_in_department:
-    if project['parentId'] == my_dept['id']:
-        try:
-            response = client.projects.get(project['id'])
-            print(f"deleting {get_project_info(response['id'])}")
-            client.projects.delete(int(response['id']))
-        except RunaiHTTPError as e:
-            print(f"An error occurred: {e}")
-print("project deletion complete")
-
-# delete the department
-client.departments.delete(my_dept['id'])
+    # create a list of projects in selected department
+    projects_in_department = []
+    projects_counter = 0
+    projects = client.projects.all()['projects']
+    for project in projects:
+        if project['parentId'] == my_dept['id']:
+            projects_in_department.append(project)
+            print(f"found {get_project_info(project['id'])}")
+            projects_counter += 0
+    print(f"found {projects_counter} projects.")
+    
+    # delete all projects from list
+    for project in projects_in_department:
+        if project['parentId'] == my_dept['id']:
+            try:
+                response = client.projects.get(project['id'])
+                print(f"deleting {get_project_info(response['id'])}")
+                client.projects.delete(int(response['id']))
+            except RunaiHTTPError as e:
+                print(f"An error occurred: {e}")
+    print("project deletion complete")
+    
+    # delete the department
+    client.departments.delete(my_dept['id'])
